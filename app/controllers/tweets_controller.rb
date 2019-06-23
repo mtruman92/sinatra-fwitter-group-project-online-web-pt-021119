65576ff136a194c59c04b64e3def68e43b4e7939
Mickae Truman <mtruman@nyit.edu>
2019-06-23 21:27:12 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,66 @@
 class TweetsController < ApplicationController
 
+ get '/tweets' do
+    if logged_in?
+    @tweets = Tweet.all
+    erb :'/tweets/index'
+    else
+      redirect '/login'
+    end
+  end
+
+   get '/tweets/new' do
+    if logged_in?
+    erb :'/tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+   get '/tweets/:id' do
+    if logged_in?
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/show'
+    else
+      redirect '/login'
+    end
+  end
+
+   get '/tweets/:id/edit' do
+    if logged_in?
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/edit'
+    else
+      redirect '/login'
+    end
+  end
+
+   post '/tweets' do
+        if params[:content] == ""
+          redirect '/tweets/new'
+        else
+          tweet = Tweet.create(:content=> params[:content])
+          tweet.user_id = current_user.id
+          tweet.save
+      end
+    end
+
+   patch '/tweets/:id/edit' do
+    tweet = Tweet.find(params[:id])
+    tweet.content = params[:content]
+    tweet.save
+  end
+
+
+
+   delete '/tweets/:id/delete' do
+    if logged_in?
+    @tweet = Tweet.find_by(params[:id])
+    @tweet.delete
+    else
+      redirect '/login'
+    end
+    redirect '/tweets'
+  end
 
 end
